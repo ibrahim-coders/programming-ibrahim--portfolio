7be11b638b6c42f8fbe8b1a034b9a40a7fd33918
programming-ibrahim <freelanceribrahim39@gmail.com>
2024-07-29 21:46:01 +0300

    portfolio showcase section add

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,6 @@ module.exports = {
   content: ['./index.html'],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Roboto', 'sans-serif'],
-      },
-      animation: {
-        type: 'type 2s infinite steps(12)',
-      },
-      keyframes: {
-        type: {
-          '40%, 60%': { left: '100%' },
-          '100%': { left: '0' },
-        },
-      },
     },
   },
   plugins: [],
